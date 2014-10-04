332f1321b8eb4dbd61a1e2f60705519d37374b34
tro3 <trey.roessig@gmail.com>
2014-10-04 13:43:22 -0700

    Making ngbp app more easily deployed

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ import os.path as path
 import json
 from PIL import Image
 from StringIO import StringIO
-from flask import Flask, render_template, request, abort, Response, Blueprint
+from flask import Flask, render_template, request, abort, Response, Blueprint, redirect
 from core import auth_layer, views
 
 class AngularFlask(Flask):
@@ -18,85 +18,47 @@ app = None
 
 def create_app(**config):
     global app
-    app = AngularFlask(__name__)
+    app = AngularFlask(__name__, static_folder='static', static_url_path='')
+    try:
+        import endpoints
+        app.config['ENDPOINTS'] = endpoints.ENDPOINTS
+    except:
+        pass
     app.config.update(config)
     app.db = auth_layer.init(config.get('CLIENT', None))
     
 
     @app.before_request
     def add_user():
-        pass
-        #if 'REMOTE_USER' not in request.environ:
-        #    if app.config['MONGO_DBNAME'] == 'e2e_test' or app.config['DEBUG']:
-        #        if path.exists('test_user.txt'):
-        #            request.user = app.mongo.db.users.find_one({'username':open('test_user.txt').read().strip()})
-        #        else:
-        #            request.user = app.mongo.db.users.find_one({'_id':1})
-        #        return
-        #    else:
-        #        abort(403)
-        #username = request.environ['REMOTE_USER'].split('\\')[1]
-        #request.user = app.mongo.db.users.find_one({'username':username})
-        #if request.user is None:
-        #    abort(403)
+        if not app.config.get('REQUIRE_USER', False):
+            return
+        if 'REMOTE_USER' in request.environ:
+            username = request.environ['REMOTE_USER'].split('\\')[1]
+        elif 'TEST_USER' in app.config:
+            username = request.environ['TEST_USER']
+        else:
+            abort(403)
+        request.user = app.db.users.find_one({'username':username})
+        if request.user is None:
+            abort(403)
 
-    
+
     #@app.errorhandler(404)
     #def not_found(error):
     #    return render_template('404.html'), 404
-    #
-    #@app.route('/')
-    #def homepage():
-    #    return render_template('index.html')
-        
+
     
-    #IMG_UPLOAD_DIR = 'app/static/uploads/images/'
-    #
-    #@app.route('/image_upload', methods=['POST'])
-    #def upload_image():
-    #    if 'file' in request.files:
-    #        try:
-    #            img = Image.open(request.files['file'])
-    #            png = StringIO()
-    #            img.save(png, 'png')
-    #            #thmb = StringIO()
-    #            #img.thumbnail((64,64), Image.ANTIALIAS)
-    #            #img.save(thmb, 'png')
-    #        except:
-    #            return Response('Could not convert file to PNG format', status=500)
-    #    else:
-    #        png = None
-    #        
-    #    if not png:
-    #        return Response('No file attached', status=500)
-    #        
-    #    ind = int(open(path.join(IMG_UPLOAD_DIR, 'ind.txt')).read())
-    #    fname = 'img%i.png' % (ind)
-    #    open(path.join(IMG_UPLOAD_DIR, fname), 'wb').write(png.getvalue())
-    #    open(path.join(IMG_UPLOAD_DIR, 'ind.txt'), 'w').write(str(ind+1))
-    #    #rev = NoteImage(session=kwords['session'])
-    #    #rev.image.save(fname,
-    #    #               ContentFile(png.getvalue()),
-    #    #               save=False)
-    #    #rev.thumb.save(fname,
-    #    #               ContentFile(thmb.getvalue()),
-    #    #               save=False)
-    #    #rev.save()
-    #    return Response(
-    #        json.dumps({
-    #            'name': IMG_UPLOAD_DIR[3:] + fname,
-    #            #'type':'f',
-    #            #'size': rev.image.size
-    #            }),
-    #        content_type = 'application/json'
-    #        )        
-        
-    #collections = autodiscover(app)
-    #autoregister(app, collections)
+    @app.route('/')
+    def homepage():
+        return redirect('/home/')
+    app.add_url_rule('/home', 'home', lambda: app.send_static_file('home/index.html'))
+    
+    
 
     app.api = Blueprint('api', 'api', url_prefix='/api')
 
-    for name, endp in config.get('ENDPOINTS', {}).items():
+    for name, endp in app.config.get('ENDPOINTS', {}).items():
+        app.add_url_rule('/%s' % name, name, lambda: app.send_static_file('%s/index.html' % name))
         create_endpoint(name, endp)
     
     app.register_blueprint(app.api)
